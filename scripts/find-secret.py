df6d655eab2ec9cb70886e1f0f95a0e7f451849c
qiaow02 <qw6991@icloud.com>
2021-06-29 15:01:50 +0800

    Clean the scripts after the installation

--- scripts/find-secret.py
+++ scripts/find-secret.py
@@ -1,12 +1,9 @@
 import json
 import sys
 jsonfile = sys.argv[1]
-resultfile = sys.argv[2]
 
 with open(jsonfile,'r') as f:
     s1 = json.load(f)
-    with open(resultfile, 'w') as f:
-       f.write(s1["SecretString"])
-with open(resultfile,'r') as f:
-    s1 = json.load(f)
-    print(s1["password"])
+    s2 = s1["SecretString"]
+    s3 = eval(s2)
+    print(s3["password"])
